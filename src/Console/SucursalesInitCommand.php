abf52d4dd98fdec2ccdcba9a58a7f8f546f398c2
Sxm <104781208+SxmHerrerx@users.noreply.github.com>
2025-04-04 15:29:56 -0600

    SucursalesInitCommand editado para uso de la nueva navegacion

--- src/Console/SucursalesInitCommand.php
+++ src/Console/SucursalesInitCommand.php
@@ -2,6 +2,7 @@
 
 namespace Ongoing\Sucursales\Console;
 
+use App\Entities\Navegacion;
 use Illuminate\Console\Command;
 use App\Repositories\NavegacionRepositoryEloquent;
 
@@ -39,15 +40,44 @@ class SucursalesInitCommand extends Command
     public function handle(NavegacionRepositoryEloquent $nav_repo)
     {
 
-        $nav_gpo = $nav_repo->firstOrCreate([
-            'descripcion' => 'Sucursales', 
-            'url' => '/sucursales', 
-            'padre_id' => 0, 
-            'permisos' => []
-        ]);
+        $menu_navegacion = [
+            [
+                'descripcion' => 'Administración operativa',
+                'url' => '/',
+                'icono' => '',
+                'orden' => 1,
+                'submenu' => [
+                    [
+                        'descripcion' => 'Sucursales',
+                        'url' => '/sucursales',
+                        'orden' => 1,
+                        'permisos' => []
+                    ]
+                ],
+            ]
+        ];
 
-        $this->info("Proceso finalizado");
-
-        return 0;
+        foreach ($menu_navegacion as $row) {
+            $submenu = $row['submenu'] ?? [];
+            unset($row['submenu']);
+            $nav = Navegacion::firstOrCreate(['url' => $row['url'], 'descripcion' => $row['descripcion']], $row);
+            $nav->fill($row)->save();
+            $ids_submenu = [];
+            foreach ($submenu as $sub) {
+                $sub_submenu = $sub['submenu'] ?? [];
+                unset($sub['submenu']);
+                $sub_nav = $nav->submenu()->firstOrCreate(['url' => $sub['url'], 'descripcion' => $sub['descripcion']], $sub);
+                $sub_nav->fill($sub)->save();
+                $ids_submenu[] = $sub_nav->id;
+                $ids_sub_sub = [];
+                foreach ($sub_submenu as $sub_sub) {
+                    $sub_sub_nav = $sub_nav->submenu()->firstOrCreate(['url' => $sub_sub['url'], 'descripcion' => $sub_sub['descripcion']], $sub_sub);
+                    $sub_sub_nav->fill($sub_sub)->save();
+                    $ids_sub_sub[] = $sub_sub_nav->id;
+                }
+                $sub_nav->submenu()->whereNotIn('id', $ids_sub_sub)->delete();
+            }
+            $nav->submenu()->whereNotIn('id', $ids_submenu)->delete();
+        }
     }
 }
